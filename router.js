451410a63919e136e92d23d086e46cb3901296e3
milyasbpa <basilyas.2015@gmail.com>
2020-05-07 14:53:22 +0700

    membuat router dan setting server.js

---
+++ router.js
@@ -0,0 +1,5 @@
+module.exports = (app) => {
+    const jsonku = require('./controller');
+    app.route('/')
+    .get(jsonku.index);
+}
--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
+// panggil router
+const router= require('./router')
+router(app);
+
 const port=4000;
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
