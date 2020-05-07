f363da63daafe9368da5802d1184ef0ceabd98b5
milyasbpa <basilyas.2015@gmail.com>
2020-05-07 15:55:13 +0700

    post action

--- controller.js
+++ controller.js
@@ -25,3 +25,17 @@ exports.tampilberdasarkanid = (req,res) => {
         }
     })
 }
+
+exports.tambahmahasiswa = (req,res) => {
+    let nim = req.body.nim;
+    let nama = req.body.nama;
+    let jurusan = req.body.jurusan;
+    connection.query('INSERT INTO mahasiswa (nim,nama,jurusan) = ?', [nim,nama,jurusan], (error,results,fields) => {
+        if (error) {
+            connection.log(error)
+        } else {
+            response.ok("Data berhasil ditambah",res)
+        }
+    })
+}
+
