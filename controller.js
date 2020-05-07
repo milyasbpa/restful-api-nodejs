c69980f69b1a28a1ccf9b401239dc99527970f7b
milyasbpa <basilyas.2015@gmail.com>
2020-05-07 15:19:29 +0700

    ngerun server

--- controller.js
+++ controller.js
@@ -2,5 +2,5 @@ const response = require('./res');
 const connection = require('./koneksi')
 
 exports.index = (req,res) => {
-    response.ok('REST API running')
+    response.ok('REST API running',res)
 }
--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 const router= require('./router')
 router(app);
 
-const port=4000;
+const port=4500;
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
 });
